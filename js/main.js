a4ae88b7f3b2b32e43009dee91394b87cd50158f
Michael Mai <mm@mai-internet.de>
2018-02-09 21:42:11 +0100

    version 1.0

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+jQuery.noConflict();
+(function ($) {
+    $(function () {
+        var shrinkHeader = 500;
+        $(window).scroll(function () {
+            var scroll = getCurrentScroll();
+            if (scroll >= shrinkHeader) {
+                $('#header').addClass('fix-area');
+                // $("#logo").find("img").attr("src", "files/ecc-logo-einzeilig.png");
+            }
+            else {
+                $('#header').removeClass('fix-area');
+                // $("#logo").find("img").attr("src", "files/Eisenschmidt_Consulting_Crew.png");
+            }
+        });
 
+        function getCurrentScroll() {
+            return window.pageYOffset || document.documentElement.scrollTop;
+        }
+    });
+    // smooth scrolling with anchor links, mi:
+    $("a[href^=#]").click(function (e) {
+        e.preventDefault();
+        var dest = $(this).attr('href');
+        $('html,body').animate({scrollTop: $(dest).offset().top}, 'slow');
+    });
+})(jQuery);
